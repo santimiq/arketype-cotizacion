7a23d49fbb896269499f9cd921dfb98b0229d67e
Santiago Miqueo <smiqueo@hotmail.com>
2020-02-05 11:39:40 -0600

    Delete column description from requirement model

---
+++ db/migrate/20200205173854_remove_field_description_from_requirement.rb
@@ -0,0 +1,6 @@
+class RemoveFieldDescriptionFromRequirement < ActiveRecord::Migration[6.0]
+  def change
+
+    remove_column :requirements, :description, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_04_224708) do
+ActiveRecord::Schema.define(version: 2020_02_05_173854) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -70,7 +70,6 @@ ActiveRecord::Schema.define(version: 2020_02_04_224708) do
   create_table "requirements", force: :cascade do |t|
     t.string "name"
     t.string "title"
-    t.string "description"
     t.bigint "quotation_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
