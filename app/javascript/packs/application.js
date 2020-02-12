a1bf1a5563851521134d59750b2fa31a6da95246
Santiago Miqueo <smiqueo@hotmail.com>
2020-02-12 11:12:04 -0600

    adding css to form with bootrsap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,3 +7,17 @@ import { definitionsFromContext } from "stimulus/webpack-helpers"
 const application = Application.start()
 const context = require.context("../controllers", true, /\.js$/)
 application.load(definitionsFromContext(context))
+
+
+const titles = document.querySelectorAll(".title")
+
+titles.forEach((title) => {
+  title.addEventListener("click", (event) => {
+    document.querySelectorAll(".firstP").forEach((element) => {
+      if (event.target.nextElementSibling.classList.contains("d-none") && !(event.target.nextElementSibling == element) ) {
+        element.classList.toggle("d-none")
+      }
+      element.classList.toggle("d-none")
+    })
+  })
+})
