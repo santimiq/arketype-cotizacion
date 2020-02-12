cba3df48d1f549996b72f5c1b30946b4a6e6da95
Santiago Miqueo <smiqueo@hotmail.com>
2020-02-12 14:24:31 -0600

    add js animation for buttons

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,13 +11,99 @@ application.load(definitionsFromContext(context))
 
 const titles = document.querySelectorAll(".title")
 
+
+const removeAll = function(node, className, hint){
+  node.forEach((e)=>{
+
+   const first =  e.children[0];
+
+    if (first.classList.contains(className) && first.classList.contains(hint)) {
+        first.classList.remove(className)
+    }
+
+  })
+};
+
+const hideAll = function() {
+  document.querySelectorAll(".firstP").forEach((e)=>{
+    e.classList.add("d-none")
+  })
+}
+
+
+
+const addActiveToI = function(e){
+  const first = e.children[0];
+    if(first.classList.contains("fa")){
+      first.classList.add("active")
+    }
+
+}
+
+const removeDNoneIfIconIsActive = function(){
+  let parent = document.querySelectorAll(".title-parent");
+  console.log(parent);
+  parent.forEach((child)=>{
+    console.log(".child")
+    child.querySelectorAll(".prueba").forEach((i)=>{
+      console.log("prueba")
+      i.querySelectorAll(".title").forEach((j)=>{
+        console.log("title")
+        if(j.children[0].classList.contains("active")){
+          console.log("title")
+          i.querySelectorAll(".firstP").forEach((k)=>{
+            k.classList.remove("d-none")
+          })
+        }
+      })
+    })
+  })
+}
+
+
+
 titles.forEach((title) => {
   title.addEventListener("click", (event) => {
+
     document.querySelectorAll(".firstP").forEach((element) => {
-      if (event.target.nextElementSibling.classList.contains("d-none") && !(event.target.nextElementSibling == element) ) {
-        element.classList.toggle("d-none")
-      }
-      element.classList.toggle("d-none")
+
+      if(event.target.classList.contains("active")){
+
+      }else{
+        removeAll(titles,"active", "fa")
+        addActiveToI(title)
+
+
+        hideAll()
+        removeDNoneIfIconIsActive()
+
+    }
     })
   })
 })
+
+
+
+
+
+
+
+
+
+
+// const titles = document.querySelectorAll(".title")
+
+// titles.forEach((title) => {
+//   title.addEventListener("click", (event) => {
+
+//     document.querySelectorAll(".firstP").forEach((element) => {
+
+//       if (event.target.nextElementSibling.classList.contains("d-none") && !(event.target.nextElementSibling == element)) {
+
+//         element.classList.toggle("d-none")
+//       }
+//       element.classList.toggle("d-none")
+//     })
+//   })
+// })
+
